bf6275d77a24c5328c452cf905c449a64d671723
octaltree <octaltree@users.noreply.github.com>
2021-05-23 17:33:59 +0900

    rm unused

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -68,19 +68,3 @@ unsafe fn from_bytes(vec: Vec<u8>) -> (*mut u8, usize) {
 mod score {
     include!(concat!(env!("OUT_DIR"), "/score.rs"));
 }
-
-//#[tonic::async_trait]
-// impl Greeter for MyGreeter {
-//    async fn say_hello(
-//        &self,
-//        request: Request<HelloRequest>
-//    ) -> Result<Response<HelloReply>, Status> {
-//        println!("Got a request: {:?}", request);
-
-//        let reply = hello_world::HelloReply {
-//            message: format!("Hello {}!", request.into_inner().name).into()
-//        };
-
-//        Ok(Response::new(reply))
-//    }
-//}
